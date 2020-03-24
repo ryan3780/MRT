88236714d21755f4b1597abdd5a60ecb1bb86018
Peter <peter@grepp.co>
2020-03-24 18:36:37 +0900

    Initial commit

---
+++ base-code/typescript/src/App.tsx
@@ -0,0 +1,15 @@
+import 'react-input-range/lib/css/index.css'
+
+import React from 'react'
+
+import HomePage from './pages/home/HomePage'
+import HotelListPage from './pages/list/HotelListPage'
+
+export default function App() {
+  return (
+    <>
+      <HomePage />
+      <HotelListPage />
+    </>
+  )
+}
---
+++ base-code/typescript/src/pages/list/HotelListPage.tsx
@@ -0,0 +1,24 @@
+import 'react-input-range/lib/css/index.css'
+
+import React, { useState } from 'react'
+import InputRange, { Range } from 'react-input-range'
+
+export default function HotelListPage() {
+  const [value, setValue] = useState({
+    min: 0,
+    max: 1000000,
+  })
+
+  return (
+    <div style={{ height: 400, width: 200, padding: 100 }}>
+      <InputRange
+        minValue={0}
+        maxValue={1000000}
+        value={value}
+        step={1000}
+        formatLabel={value => (value === 1000000 ? '1,000,000+' : value.toString())}
+        onChange={value => setValue(value as Range)}
+      />
+    </div>
+  )
+}
