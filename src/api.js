d4d5795a46f683bfc0a2d1d9fb129599e9434b0c
ryan3780 <45477679+ryan3780@users.noreply.github.com>
2020-04-14 17:05:26 +0900

    재택 근무 ... 꼭 가야한다

--- src/api.js
+++ src/api.js
@@ -1,20 +1,32 @@
-const TheHotelAPI = "https://x0ofq07ykl.execute-api.ap-northeast-2.amazonaws.com/dev/hotels";
+const TheHotelAPI =
+  "https://x0ofq07ykl.execute-api.ap-northeast-2.amazonaws.com/dev/hotels";
 
 const callAPI = url => {
-    return fetch(url)
-        .then(response => response.json())
-        .then(data => data)
-        .catch(err => console.log(err));
+  return fetch(url)
+    .then(response => response.json())
+    .then(data => data)
+    .catch(err => console.log(err));
 };
 
 export const requestImages = async setData => {
-    try {
-        const result = await callAPI(TheHotelAPI);
-        setData(result)
-        return result;
-    } catch (e) {
-        console.warn(e);
-    }
+  try {
+    const result = await callAPI(TheHotelAPI);
+    setData(result);
+    return result;
+  } catch (e) {
+    console.warn(e);
+  }
+};
+
+export const getPrices = async id => {
+  const Prices = `https://x0ofq07ykl.execute-api.ap-northeast-2.amazonaws.com/dev/hotel-prices?ids=${id}`;
+  try {
+    const result = await callAPI(Prices);
+    console.log(result);
+    return result;
+  } catch (e) {
+    console.warn(e);
+  }
 };
 
 // export const requestMoreCats = async () => {
@@ -27,7 +39,7 @@ export const requestImages = async setData => {
 //     try {
 //         const cats = await callAPI(TheCatAPI);
 //         console.log(cats)
-//         if (cats.length) 
+//         if (cats.length)
 //             setImages({images: cats, loading: false})
 //         return cats;
 //     } catch (e) {
--- src/pages/list/HotelListPage.jsx
+++ src/pages/list/HotelListPage.jsx
@@ -1,73 +1,115 @@
-import 'react-input-range/lib/css/index.css'
+import "react-input-range/lib/css/index.css";
 
-import React, { useState } from 'react'
-import InputRange from 'react-input-range'
-import './HotelListPage.css'
-import { requestImages } from '../../api'
+import React, { useState } from "react";
+import InputRange from "react-input-range";
+import "./HotelListPage.css";
+import { requestImages, getPrices } from "../../api";
 
 export default function HotelListPage() {
   const [value, setValue] = useState({
     min: 0,
-    max: 1000000,
-  })
-  const [data, setData] = useState([])
+    max: 1000000
+  });
+  const [data, setData] = useState([]);
+  console.log(getPrices(5));
 
-  const getHotels = () =>{
-    setData(requestImages(setData))
-  }
-  console.log(data)
+  const getHotels = () => {
+    setData(requestImages(setData));
+  };
+  // console.log(data);
 
-  const filteredHotel = []
-  data.length > 0 && data.map((ele, idx) =>{
-    filteredHotel.push(
-      <div key={idx} style={{ display: `flex`, backgroundColor : `#fff`, marginBottom : `10px`}}>
-        <img src={ele.imageUrl} alt={ele.name} style={{flex : 0.75}} />
-        <div style={{ flex : 1.25, marginTop : `20px`}}> {ele.name}</div>
-        <div style={{marginTop : `20px`}}>{ele.totalReviewCount}개의 이용 후기 <button>{ele.rate}</button></div>
-      </div>
-    )
-    return filteredHotel
-  })
+  let serve = [];
+  const test = services => {
+    serve = [];
+    services.forEach(ele => {
+      serve.push(<button>{ele}</button>);
+    });
+    return serve;
+  };
+  const eachPrices = [];
+  const prices = id => {
+    eachPrices.push(<div>{getPrices(id)}</div>);
+  };
+
+  const filteredHotel = [];
+  data.length > 0 &&
+    data.map((ele, idx) => {
+      filteredHotel.push(
+        <div
+          key={idx}
+          style={{
+            display: `flex`,
+            backgroundColor: `#fff`,
+            marginBottom: `10px`
+          }}
+        >
+          <img src={ele.imageUrl} alt={ele.name} style={{ flex: 0.75 }} />
+          <div style={{ flex: 1.25, marginTop: `20px`, marginLeft: `20px` }}>
+            {ele.name}
+            <div style={{ marginTop: `10px` }}>{test(ele.freeServices)}</div>
+            <div style={{ marginTop: `10px` }}>{ele.rate}성급 호텔</div>
+          </div>
+          <div style={{ marginTop: `20px` }}>
+            {ele.totalReviewCount}개의 이용 후기{" "}
+            <button>{ele.reviewScore}</button>
+          </div>
+          <div>{prices(ele.id)}</div>
+        </div>
+      );
+      return filteredHotel;
+    });
 
   return (
-  <div className="wrapper">
-  <div className="one">필터
-  <button onClick={getHotels}>Test</button>
-  </div>
-  
-  <div className="two">최근 본 호텔</div>
-  <div className="three">
-  <h3>요금</h3>
-      <InputRange
-        minValue={0}
-        maxValue={1000000}
-        value={value}
-        step={1000}
-        formatLabel={value => (value === 1000000 ? '1,000,000+' : value.toString())}
-        onChange={value => setValue(value)}
-      />
-  <h3>리뷰 점수</h3>
-      <InputRange
-        minValue={0}
-        maxValue={10}
-        value={value}
-        step={1}
-        formatLabel={value => (value === 10 ? '10' : value.toString())}
-        onChange={value => setValue(value)}
-      />
-    <h3>시설 정보</h3>
-    <div>
-    <label><input type='checkbox' />FREE-WIFI</label>
+    <div className="wrapper">
+      <div className="one">
+        필터
+        <button onClick={getHotels}>Test</button>
+      </div>
+
+      <div className="two">최근 본 호텔</div>
+      <div className="three">
+        <h3>요금</h3>
+        <InputRange
+          minValue={0}
+          maxValue={1000000}
+          value={value}
+          step={1000}
+          formatLabel={value =>
+            value === 1000000 ? "1,000,000+" : value.toString()
+          }
+          onChange={value => setValue(value)}
+        />
+        <h3>리뷰 점수</h3>
+        <InputRange
+          minValue={0}
+          maxValue={10}
+          value={value}
+          step={1}
+          formatLabel={value => (value === 10 ? "10" : value.toString())}
+          onChange={value => setValue(value)}
+        />
+        <h3>시설 정보</h3>
+        <div>
+          <label>
+            <input type="checkbox" />
+            FREE-WIFI
+          </label>
+        </div>
+        <div>
+          <label>
+            <input type="checkbox" />
+            FREE-PARKING
+          </label>
+        </div>
+        <div>
+          <label>
+            <input type="checkbox" />
+            FREE-AIRPORT-PICKUP
+          </label>
+        </div>
+      </div>
+      <div className="four"></div>
+      <div className="five">{filteredHotel}</div>
     </div>
-    <div>
-    <label><input type='checkbox' />FREE-PARKING</label>
-    </div>
-    <div>
-    <label><input type='checkbox' />FREE-AIRPORT-PICKUP</label>
-    </div>
-  </div>
-  <div className="four">Four</div>
-  <div className="five">{filteredHotel}</div>
-</div>
-  )
+  );
 }
